a7e10cdf83310e9c66405515184f0fe82e31c489
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2023-06-15 09:36:50 +0200

    Big Bump

--- README.md
+++ README.md
@@ -1,21 +1,124 @@
-# Fallout-76-Nuke-Codes-API-Node.js
-some basic lines of code in node.js that read the https://nukacrypt.com/home web page and returns all the nuke code data.
-<br><hr>
+# NukaCrypt API
 
-<hr />
-<span> Example Code </span>
-<hr />
-<br>
-first install the dependency<br />
-<code>npm install node-fetch</code>
-<br />
-<br />
-and then all thats left is the code thats in this file<br /><br />
-code: <a href='https://github.com/SkellyM386/Fallout-76-Nuke-Codes-API-Node.js/blob/main/index.js'>index.js</a>
+## THIS PROJECT IS STILL IN DEVELOPMENT. 
+## Documentation is not complete and the API is not yet ready for production use.
 
-<hr />
-if you have any questions feel free to email me at <a href="https://mail.google.com/mail/?tab=rm&authuser=0&ogbl">skellymclane@gmail.com</a>
-<br />
-<hr />
-code powered by Echsarah on discord the owner of Nukacrypt.
+# Description
+This repository contains a Node.js application that interacts with the NukaCrypt API to retrieve data about nuclear launch codes. The application is built using Express.js and Puppeteer, and it utilizes Redis for caching the retrieved data.
 
+## Why Redis?
+
+The NukaCrypt API is in reality the whole NukaCrypt site, which returns the Nuke Codes via GraphQL request.
+
+This is very inefficient, as the application has to load the whole site and then parse the data to get the Nuke Codes.
+This can be a problem if you have a lot of users accessing the API at the same time. 
+
+To solve this problem, the application caches the retrieved data in Redis. 
+This way, the application can serve the data from the cache instead of making a request to the NukaCrypt API every time a user accesses the API.
+
+## Prerequisites
+
+Before running the application, ensure you have the following dependencies installed:
+
+- Node.js v16
+- Redis (*optional if you have a Redis server somewhere else*)
+
+## Installation
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/lyzcoote/Fallout-76-Nuke-Codes-API-Node.js.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+    cd Fallout-76-Nuke-Codes-API-Node.js
+   ```
+
+3. Install the required dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Configuration
+
+The application requires Redis to be running. 
+Make sure Redis is installed and running on your system. 
+
+Although is **recommended** to host a Redis server somewhere else (like the [Redis Cloud](https://redis.com/try-free/)).
+
+After that, update the Redis configuration in the `index.js` file:
+
+```javascript
+const redisClient = redis.createClient({
+    password: 'REDIS-PASSWORD',
+    socket: {
+        host: 'REDIS-HOST',
+        port: 'REDIS-PORT'
+    }
+});
+```
+
+Replace the values for `REDIS-PASSWORD`, `REDIS-HOST`, and `REDIS-PORT` with your Redis configuration.
+
+## Usage
+
+First, you need to install the dependencies by running the following command:
+
+```bash
+npm install
+```
+
+After that, start the application, run the following command:
+
+```bash
+npm run server
+```
+
+The application will start an Express server on port 80. You can access the API by navigating to `http://localhost` in your browser or using a tool like cURL or Postman.
+
+The available API endpoint is `/`, which returns the nuclear launch codes retrieved from NukaCrypt or the Redis cache.
+
+## API Documentation
+
+### Request
+
+- URL: `/`
+- Method: `GET`
+- Headers:
+    - `no-cache` (optional): Set to `'true'` to bypass the Redis cache and fetch the data directly from NukaCrypt.
+
+### Response
+
+The API response is in JSON format and contains the following properties:
+
+- `Alpha`: The alpha launch code.
+- `Bravo`: The bravo launch code.
+- `Charlie`: The charlie launch code.
+- `ResetsIn`: The time until the launch codes reset.
+- `RenewalTime`: The estimated time of the next launch code reset.
+- `Cached`: Indicates whether the response was retrieved from the Redis cache (`true`) or fetched from NukaCrypt (`false`).
+- `PoweredBy`: The technology used to retrieve the data (`Redis` or `Puppeteer`).
+- `isTimeAprox`: Indicates whether the renewal time is an approximation (`true`) or an exact time (`false`).
+
+Example response:
+
+```json
+{
+    "Alpha": "59192086",
+    "Bravo": "62667842",
+    "Charlie": "05091983",
+    "ResetsIn": "2d 17h",
+    "RenewalTime": "18/06/2023, 02:00:00",
+    "Cached": true,
+    "PoweredBy": "Redis",
+    "isTimeAprox": true
+}
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+// ########################################################
+// #                                                      #
+// #                      Init Phase                      #
+// #                                                      #
+// ########################################################
+
+// Get libraries and modules
 const cheerio = require('cheerio');
 const express = require('express');
 const puppeteer = require('puppeteer');
 const redis = require('redis');
 
+// Initialize Express and Redis
 const app = express();
+
+// Setup Redis
+const start = async () => {
+    await redisClient.connect();
+};
 const redisClient = redis.createClient({
     password: 'FurryYiff69!',
     socket: {
@@ -12,10 +25,13 @@ const redisClient = redis.createClient({
     }
 });
 
-const start = async () => {
-    await redisClient.connect();
-};
+// ########################################################
+// #                                                      #
+// #                    Code Execution                    #
+// #                                                      #
+// ########################################################
 
+// Connect to Redis and handle errors
 start().then(() => {
     console.log('Connected to Redis');
 }).catch(error => {
@@ -23,6 +39,7 @@ start().then(() => {
     process.exit(1);
 });
 
+// Log requests to console for debugging reasons, this will be replaced with a proper logger later
 app.use((req, res, next) => {
     const timestamp = new Date().toLocaleString();
     const userAgent = req.headers['user-agent'];
@@ -32,27 +49,34 @@ app.use((req, res, next) => {
     next();
 });
 
+// Main API request handler
 app.get('/', async (req, res) => {
     try {
+        // Get cache values
         const cacheKeys = ['Alpha', 'Bravo', 'Charlie', 'RenewalTime'];
         const cacheValues = await Promise.all(cacheKeys.map(key => redisClient.get(key)));
         const cacheResetsIn = await redisClient.get('ResetsIn');
-
         let cacheResetsInMatch = null;
-        try {
+
+        // Try to parse the ResetsIn value from Redis via Regex
+        try
+        {
             const regex = /(\d{1,2}d\s\d{1,2}h)/g;
             const matches = regex.exec(cacheResetsIn);
             cacheResetsInMatch = matches ? matches[0] : null;
-        } catch (err) {
+        }
+        catch (err)
+        {
             console.warn('FAILED TO RUN REGEX! Error:\n', err);
         }
 
+        // If the client requested no-cache, fetch manually from NukaCrypt without saving to Redis
         if (req.headers['no-cache'] === 'true')
         {
             console.log("Client requested no-cache, fetching from NukaCrypt");
             await getFromNukaCrypt(req, res, force = true);
         }
-        else if (cacheValues.every(value => value !== null))
+        else if (cacheValues.every(value => value !== null)) // If all cache values are not null, return them from the Redis cache
         {
             console.log('Retrieved data from cache');
 
@@ -69,21 +93,22 @@ app.get('/', async (req, res) => {
 
             res.json(response);
         }
-        else
+        else // If any of the cache values are null, fetch from NukaCrypt and save to Redis
         {
             await getFromNukaCrypt(req, res, force = false);
         }
     }
-    catch (error) {
+    catch (error) { // If anything goes wrong, return a 500 error
         console.error(error);
         res.status(500).json({ result: 'error', error: error.message });
     }
 });
 
-app.get('/loaderio-318019772b93552616e185652243807f', async (req, res) => {
-    const file = `${__dirname}/loaderio-318019772b93552616e185652243807f.txt`;
-    res.download(file); // Set disposition and send it.
-});
+// ########################################################
+// #                                                      #
+// #                       Functions                      #
+// #                                                      #
+// ########################################################
 
 function parseResetTime(resetsIn) {
     const parts = resetsIn.split(' ');
@@ -113,20 +138,25 @@ function calculateRenewalTime(currentTime, resetsInTime) {
     return renewalTime.toLocaleString('it-IT', options);
 }
 
+// Start Express
 app.listen(80, () => {
     console.log('Server is running on port 80');
 });
 
+// Handle SIGINT (Ctrl+C) and disconnect from Redis
 process.on('SIGINT', async () => {
     console.log('Received SIGINT. Calling save function...');
     await redisClient.disconnect();
     process.exit(0);
 });
 
+// Get data from NukaCrypt
 async function getFromNukaCrypt(req, res, force) {
     console.log('Retrieving data from NukaCrypt...\nTHIS MAY TAKE A WHILE!');
 
+    // Main try block
     try {
+        // Setup Puppeteer
         const browser = await puppeteer.launch({
             args: [
                 '--headless=new',
@@ -137,22 +167,24 @@ async function getFromNukaCrypt(req, res, force) {
             ],
         });
 
+        // Get data from NukaCrypt
         const page = await browser.newPage();
         await page.goto('https://nukacrypt.com/home');
         const html = await page.content();
         await browser.close();
 
+        // Scrap data from NukaCrypt via Cheerio
         const $ = cheerio.load(html);
         const alphaCode = $('.pl3:nth-child(1) > div:nth-child(2)').text().trim();
         const bravoCode = $('.pl3:nth-child(2) > div:nth-child(2)').text().trim();
         const charlieCode = $('.pl3:nth-child(3) > div:nth-child(2)').text().trim();
         const resetsIn = $('.flex-auto > div > span').text().trim();
-
         const resetsInTime = resetsIn ? parseResetTime(resetsIn) : null;
         const renewalTime = resetsInTime ? calculateRenewalTime(new Date(), resetsInTime) : null;
 
         console.log('Retrieved data from NukaCrypt is:\n', alphaCode, bravoCode, charlieCode, resetsIn, renewalTime);
 
+        // Setup response
         const response = {
             Alpha: alphaCode,
             Bravo: bravoCode,
@@ -161,6 +193,7 @@ async function getFromNukaCrypt(req, res, force) {
             RenewalTime: renewalTime
         };
 
+        // Save to Redis if not forced
         if(!force)
         {
             // Saving to Redis
@@ -176,13 +209,14 @@ async function getFromNukaCrypt(req, res, force) {
             }
         }
 
-
+        // Return response
         response.Cached = false;
         response.PoweredBy = 'Puppeteer';
         response.isTimeAprox = false;
 
         res.json(response);
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
         res.status(500).json({ result: 'error', error: error.message });
     }
